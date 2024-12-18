1de30bb36bd1d0fa7aee6d23da22a27a21600002
Brian Ruan <brian.ruan@convergentimaging.com>
2024-12-18 18:16:55 -0500

    Completed part two of the day two challenger.

--- day2/index.js
+++ day2/index.js
@@ -3,36 +3,86 @@ import fs from "fs";
 // Part 1
 console.log("Calculating the answer for day 2 part 1...");
 
-const input = fs.readFileSync("./day2/input.txt", "utf-8");
-const lines = input.split("\n");
+const input = fs
+  .readFileSync("./day2/input.txt", "utf-8")
+  .split("\n")
+  .map((line) => line.split(" "));
+
+function isStrictlyIncreasingWithTolerance(line, limit) {
+  let prev = line[0];
+
+  for (let i = 1; i < line.length; i++) {
+    let cur = line[i];
+    const diff = Number(cur) - Number(prev);
+
+    if (diff > 3 || diff <= 0) {
+      if (limit === 0) return 0;
+      return Math.max(
+        isStrictlyIncreasingWithTolerance(
+          [...line.slice(0, i - 1), ...line.slice(i)],
+          limit - 1
+        ),
+        isStrictlyIncreasingWithTolerance(
+          [...line.slice(0, i), ...line.slice(i + 1)],
+          limit - 1
+        )
+      );
+    }
+
+    prev = cur;
+  }
+
+  return 1;
+}
+
+function isStrictlyDecreasingWithTolerance(line, limit) {
+  let prev = line[0];
+
+  for (let i = 1; i < line.length; i++) {
+    let cur = line[i];
+    const diff = Number(prev) - Number(cur);
+
+    if (diff > 3 || diff <= 0) {
+      if (limit === 0) return 0;
+      return Math.max(
+        isStrictlyDecreasingWithTolerance(
+          [...line.slice(0, i - 1), ...line.slice(i)],
+          limit - 1
+        ),
+        isStrictlyDecreasingWithTolerance(
+          [...line.slice(0, i), ...line.slice(i + 1)],
+          limit - 1
+        )
+      );
+    }
+
+    prev = cur;
+  }
+
+  return 1;
+}
 
 let nSafeReports = 0;
 
-lines.forEach((line) => {
-  const nums = line.split(" ");
-  let isSafe = true;
-  let isAscending = false;
-
-  for (let i = 1; i < nums.length; i++) {
-    const diff = Number(nums[i]) - Number(nums[i - 1]);
-
-    if (i === 1) isAscending = diff > 0;
-
-    if (
-      Math.abs(diff) > 3 ||
-      diff === 0 ||
-      (isAscending && diff < 0) ||
-      (!isAscending && diff > 0)
-    ) {
-      isSafe = false;
-      break;
-    }
-  }
-
-  if (isSafe) nSafeReports++;
+input.forEach((line) => {
+  nSafeReports += Math.max(
+    isStrictlyIncreasingWithTolerance(line, 0),
+    isStrictlyDecreasingWithTolerance(line, 0)
+  );
 });
 
 console.log(`The answer for day 2 part 1 is: ${nSafeReports}.\n`);
 
 // Part 2
 console.log("Calculating the answer for day 2 part 2...");
+
+let nSafeReportsWithTolerate = 0;
+
+input.forEach((line) => {
+  nSafeReportsWithTolerate += Math.max(
+    isStrictlyIncreasingWithTolerance(line, 1),
+    isStrictlyDecreasingWithTolerance(line, 1)
+  );
+});
+
+console.log(`The answer for day 2 part 2 is: ${nSafeReportsWithTolerate}.\n`);
