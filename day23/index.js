ce2a6bdd2414e19eef13ee3a0432253d0b314ec8
Neon-Light0 <brianruan90@gmail.com>
2025-12-11 20:57:12 -0500

    completed day 23.

--- day23/index.js
+++ day23/index.js
@@ -9,24 +9,23 @@ for (let i = 0; i < links.length; i++){
     comps.add(links[i][1]);
 
     if (edgeMap.has(links[i][0])){
-        edgeMap.get(links[i][0]).push(links[i][1]);
+        edgeMap.get(links[i][0]).add(links[i][1]);
     } else {
-        const arr = new Array();
-        arr.push(links[i][1]);
-        edgeMap.set(links[i][0], arr);
+        const set = new Set();
+        set.add(links[i][1])
+        edgeMap.set(links[i][0], set);
     }
 
     if (edgeMap.has(links[i][1])){
-        edgeMap.get(links[i][1]).push(links[i][0]);
+        edgeMap.get(links[i][1]).add(links[i][0]);
     } else {
-        const arr = new Array();
-        arr.push(links[i][0]);
-        edgeMap.set(links[i][1], arr);
+        const set = new Set();
+        set.add(links[i][0]);
+        edgeMap.set(links[i][1], set);
     }
 }
 
 const connections = new Set();
-const visited = new Set();
 comps = [...comps].sort();
 
 for (let i = 0; i < comps.length - 2; i++){
@@ -34,7 +33,7 @@ for (let i = 0; i < comps.length - 2; i++){
         for (let k = j + 1; k < comps.length; k++){
             if (comps[i][0] === 't' || comps[j][0] === 't' || comps[k][0] === 't'){
 
-                if (edgeMap.has(comps[i]) && edgeMap.get(comps[i]).includes(comps[j]) && edgeMap.has(comps[j]) && edgeMap.get(comps[j]).includes(comps[k]) && edgeMap.has(comps[k]) && edgeMap.get(comps[k]).includes(comps[i])){
+                if (edgeMap.has(comps[i]) && edgeMap.get(comps[i]).has(comps[j]) && edgeMap.has(comps[j]) && edgeMap.get(comps[j]).has(comps[k]) && edgeMap.has(comps[k]) && edgeMap.get(comps[k]).has(comps[i])){
                     const entry = [comps[i], comps[j], comps[k]].sort();
                     connections.add(entry[0] + '-' + entry[1] + '-' + entry[2]);
                 }
@@ -43,4 +42,52 @@ for (let i = 0; i < comps.length - 2; i++){
     }   
 }
 
-console.table([...connections].length);
+console.log(`The answer for part 1 is: ${[...connections].length}.`);
+
+//Part 2
+function findOthers(visitedComps, curComp){
+    for (const connectedComp of edgeMap.get(curComp)){
+        let isConnected = true;
+        
+        for (const comp of visitedComps){
+            if (!edgeMap.get(connectedComp).has(comp)){
+                isConnected = false;
+                break;
+            }
+        }
+
+        if (isConnected){
+            visitedComps.add(connectedComp);
+            findOthers(visitedComps, connectedComp)
+        }
+    }
+}
+
+let curMax = 0;
+let solution;
+
+for (const comp of comps){
+    const set = new Set();
+    findOthers(set, comp);
+    if (set.size > curMax){
+        curMax = set.size;
+        solution = set;
+    }
+}
+
+let answer = "";
+const answerArr = [...solution].toSorted();
+
+for (let i = 0; i < answerArr.length; i++){
+    answer += answerArr[i];
+    if (i != answerArr.length - 1){
+        answer += ',';
+    }
+}
+
+console.log(`The answer for part 2 is ${answer}.`);
+
+
+
+
+
